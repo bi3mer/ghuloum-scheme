e7a9fdb452313b228d8690acb3a4c50363894bc9
Colan Biemer <bi3mer93@gmail.com>
2026-03-23 09:23:21 -0400

    step 2 of 24 complete

--- bin/main.ml
+++ bin/main.ml
@@ -1,10 +1,16 @@
+let fixnum_mask = 3
+let fixnum_tag = 0
+let fixnum_shift = 2
+
 let compile num =
   let f = open_out "out.s" in
-  output_string f ".global scheme_entry\n";
-  output_string f "scheme_entry:\n";
-  Printf.fprintf f "\tmovl $%d, %%eax\n" num;
-  output_string f "\tret\n";
-  close_out f
+  Fun.protect ~finally: (fun() -> close_out f) (fun () ->
+    output_string f ".global scheme_entry\n";
+    output_string f "scheme_entry:\n";
+    Printf.fprintf f "\tmovl $%d, %%eax\n" (num lsl fixnum_shift);
+    output_string f "\tret\n";
+    close_out f
+  )
 
 let () = if Array.length Sys.argv < 2 then
   failwith "Usage: program <number>"
--- startup.c
+++ startup.c
@@ -1,8 +1,17 @@
 #include <stdio.h>
 
+#define fixnum_mask 3
+#define fixnum_tag 0
+#define fixnum_shift 2
+
 extern int scheme_entry(void);
 
 int main(int argc, char *argv[]) {
-  printf("%d\n", scheme_entry());
+  int val = scheme_entry();
+  if ((val & fixnum_mask) == fixnum_tag) {
+    printf("%d\n", val >> fixnum_shift);
+  } else {
+    puts("()");
+  }
   return 0;
 }
